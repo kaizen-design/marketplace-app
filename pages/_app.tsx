b95bb7e39266912726fb87e707b9f397debab02f
denis <denis@kaizen-design.ru>
2022-08-03 16:15:31 +0400

    Query Data with GraphQL in Next.js from a Fauna Database

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Nav from '../components/Nav';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './gqlClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Nav />
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Nav />
+        <Component {...pageProps} />
+      </ApolloProvider>  
     </>
   )
 }
