63e590312a27c977abbb08b16e05c1ef27bf520c
denis <denis@kaizen-design.ru>
2022-08-05 17:55:50 +0400

    Implement Cart Functionality for a Store with React Context

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,23 @@
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0';
+import { useContext } from "react";
+import { Context } from "../context";
 
 const navLinkStyle = `cursor-pointer inline-flex items-center px-1 pt-1 text-gray-500 hover:text-indigo-400`
 
-const iconButtonStyle = `flex bg-white p-2 rounded-full text-gray-400 hover:text-indigo-400 border-2 border-gray-500 hover:border-indigo-400`
+const iconButtonStyle = `flex bg-white p-2 px-4 rounded-full text-gray-400 hover:text-indigo-400 border-2 border-gray-500 hover:border-indigo-400`
 
 export default function Nav() {
   const { user, isLoading } = useUser();
-  console.log(user)
+  const { state } = useContext(Context as any);
+  
+  const { cart } = state;
+  let itemCount = 0;
+
+  for (const [key, value] of Object.entries(cart)) {
+    itemCount = itemCount + cart[key].qty
+  }
+
   return (
     <nav className="bg-white shadow">
       <div className="max-w-7xl mx-auto px-4">
@@ -23,10 +33,10 @@ export default function Nav() {
           <div className="ml-6 flex items-center">
             <Link href="/cart">
               <button className={iconButtonStyle}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                 </svg>
-                {` ${3} Item(s)`}
+                {` ${itemCount} Item${itemCount !== 1 ? 's' : ''}`}
               </button>
             </Link>
             { 
--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { Context } from "../context";
+
 const ProductList = ({ products }: { products: any[] } ) => { 
   return (
     <div className={listStyle}>
@@ -10,31 +13,36 @@ const ProductList = ({ products }: { products: any[] } ) => {
   )
 }
 
-const ProductItem = ({ product } : { product: any }) => (
-  <a href="#" className="group">
-    <div className={itemContainer}>
-      <img 
-        src={product.imageUrl ? product.imageUrl : defaultImg} 
-        className={imageStyle} 
-        alt={product.name} />
+const ProductItem = ({ product } : { product: any }) => {
+  const { dispatch } = useContext(Context as any);
+  return (
+    <div className="group">
+      <div className={itemContainer}>
+        <img 
+          src={product.imageUrl ? product.imageUrl : defaultImg} 
+          className={imageStyle} 
+          alt={product.name} />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">
+        {product.name}
+      </h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">
+        ${product.price}
+      </p>
+      <button className={`${btnClass} mt-4`} onClick={() => {
+        dispatch({ type: 'ADD_TO_CART', payload: product })
+      }}>
+        Add to Cart
+        </button>
     </div>
-    <h3 className="mt-4 text-sm text-gray-700">
-      {product.name}
-    </h3>
-    <p className="mt-1 text-lg font-medium text-gray-900">
-      ${product.price}
-    </p>
-  </a>
-)
+  )
+} 
 
 const defaultImg = "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg";
-
 const listStyle = `mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8`;
-
 const listGridStyle = `grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8`;
-
 const itemContainer = `aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200`;
-
 const imageStyle = `h-full w-full object-cover object-center group-hover:opacity-75`;
+const btnClass = `my-1 rounded-md bg-indigo-100 px-4 py-2 text-indigo-700 hover:bg-indigo-200 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`;
 
 export default ProductList;
