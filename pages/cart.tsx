345f6bf1babf0ad42ecdcb1251ab323e863c0691
denis <denis@kaizen-design.ru>
2022-08-05 20:08:05 +0400

    Base URL env var fix

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -28,7 +28,7 @@ export default function Cart() {
           onClick={(e: any) => {
             e.preventDefault();
             fetch(
-              `/api/create-checkout-session`,
+              `${process.env.NEXT_PUBLIC_BASE_URL}/api/create-checkout-session`,
               {
                 method: 'POST',
                 body: JSON.stringify({ cart })
